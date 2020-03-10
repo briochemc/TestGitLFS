3dc4f737290b4cf027f65b3e24f7af2fdd67ce9d
Benoit Pasquier <briochemc@gmail.com>
2020-03-10 16:21:28 +1100

    Add pdf images

--- src/TestGitLFS.jl
+++ src/TestGitLFS.jl
@@ -3,7 +3,7 @@ using Plots
 
 peaks(x,y) = @. 3(1 - x)^2 * exp(-x^2 - (y + 1)^2) - 10(x / 5 - x^3 - y^5) * exp(-x^2 - y^2) - exp(-(x+1)^2 - y^2) / 3
 
-function savemysimpleplot(;nx=1000, ny=1000, nx2=100, saveit=false)
+function savemysimpleplot(;nx=1000, ny=1000, nx2=100, saveit=false, ext=:svg)
     x = range(-3, 3, length=nx)
     y = range(-2.5, 2.5, length=ny)
     z = peaks(x, y')
@@ -12,11 +12,11 @@ function savemysimpleplot(;nx=1000, ny=1000, nx2=100, saveit=false)
     x2 = range(-3, 3, length=nx2)
     scatter!(plt, x2, sin.(x2), zcolor=maximum(abs.(z)) * cos.(x2))
 
-    saveit && savefig("fig/test_nolfs.svg")
+    saveit && savefig("fig/test.$ext")
 
     return plt
 end
 
-savemysimpleplot(saveit=true)
+savemysimpleplot(saveit=true, ext=:pdf)
 
 
